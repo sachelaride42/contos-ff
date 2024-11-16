e40896cde1731f5d65e2c33d820412689030a14e
sachelaride42 <mateus.sachelaride@gmail.com>
2024-11-16 19:08:41 -0400

    Finalização dos testes

--- projeto/tests/RouterTest.php
+++ projeto/tests/RouterTest.php
@@ -39,6 +39,9 @@ class RouterTest extends TestCase
         $output = ob_get_clean();
 
         $this->assertStringContainsString("sucesso", $output);
+        unset($_SERVER['REQUEST_METHOD']);
+        unset($_SERVER['REQUEST_URI']);
+        unset($_SERVER['HTTP_CONTENT_TYPE']);
     }
 
     public function testUserControllerRegister(){
@@ -61,7 +64,9 @@ class RouterTest extends TestCase
         $output = ob_get_clean();
 
         $this->assertStringContainsString("sucesso", $output);
-
+        unset($_SERVER['REQUEST_METHOD']);
+        unset($_SERVER['REQUEST_URI']);
+        unset($_SERVER['HTTP_CONTENT_TYPE']);
     }
     private function simulateInput($data)
     {
@@ -72,5 +77,88 @@ class RouterTest extends TestCase
         fclose($inputStream);
     }
 
+    public function testUserControllerIsLogged(){
+        $_SERVER['REQUEST_METHOD'] = "GET";
+        $_SERVER['REQUEST_URI'] = "/isLogged";
+        if (session_status() === PHP_SESSION_NONE){
+            session_start();
+        }
+        $_SESSION['isLogged'] = true;
+        ob_start();
+        $router = new Router();
+        $router->route();
+        $output = ob_get_clean();
+        $this->assertStringContainsString("sucesso", $output);
+        session_unset();
+        session_destroy();
+    }
 
+    public function testUserControllerShow()
+    {
+        $_SERVER['REQUEST_METHOD'] = "GET";
+        $_SERVER['REQUEST_URI'] = "/api/usuarios/1";
+        ob_start();
+        $router = new Router();
+        $router->route();
+        $output = ob_get_clean();
+        $this->assertStringContainsString("sucesso", $output);
+    }
+
+    public function testUserControllerShowError(){
+        $_SERVER['REQUEST_METHOD'] = "GET";
+        $_SERVER['REQUEST_URI'] = "/api/usuarios/30000";
+        ob_start();
+        $router = new Router();
+        $router->route();
+        $output = ob_get_clean();
+        $this->assertStringContainsString("erro", $output);
+    }
+
+    public function testContoControllerIndex()
+    {
+        $_SERVER['REQUEST_METHOD'] = "GET";
+        $_SERVER['REQUEST_URI'] = "/api/contos";
+        ob_start();
+        $router = new Router();
+        $router->route();
+        $output = ob_get_clean();
+        $this->assertStringContainsString("sucesso", $output);
+    }
+
+    public function testContoControllerShow(){
+        $_SERVER['REQUEST_METHOD'] = "GET";
+        $_SERVER['REQUEST_URI'] = "/api/contos/3";
+        ob_start();
+        $router = new Router();
+        $router->route();
+        $output = ob_get_clean();
+        $this->assertStringContainsString("sucesso", $output);
+    }
+    public function testContoControllerShowError(){
+        $_SERVER['REQUEST_METHOD'] = "GET";
+        $_SERVER['REQUEST_URI'] = "/api/contos/30000";
+        ob_start();
+        $router = new Router();
+        $router->route();
+        $output = ob_get_clean();
+        $this->assertStringContainsString("erro", $output);
+    }
+    public function testContoControllerCreate()
+    {
+        $_SERVER['REQUEST_METHOD'] = "POST";
+        $_SERVER['REQUEST_URI'] = "/api/contos";
+        $_SERVER['HTTP_CONTENT_TYPE'] = 'application/json';
+        $data = json_encode([
+            'usuario_id' => 3,
+            'titulo' => "Plantação de frutas medievais",
+            'data_publicacao' => "2000-01-01",
+            'texto' => "Era uma vez um homem que plantava frutas medievais."
+        ]);
+        file_put_contents("php://input", $data);
+        ob_start();
+        $router = new Router();
+        $router->route();
+        $output = ob_get_clean();
+        $this->assertStringContainsString("sucesso", $output);
+    }
 }
